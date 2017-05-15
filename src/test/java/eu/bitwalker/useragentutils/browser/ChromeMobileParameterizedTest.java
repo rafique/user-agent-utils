824a29e1a1b754d8ea34bfd1e8dbffaf30e0ccd1
kirill <kirill@adkernel.com>
2017-05-15 13:05:21 +0300

    Android Web View

--- src/test/java/eu/bitwalker/useragentutils/browser/ChromeMobileParameterizedTest.java
+++ src/test/java/eu/bitwalker/useragentutils/browser/ChromeMobileParameterizedTest.java
@@ -6,8 +6,7 @@ import java.util.Collection;
 
 import org.junit.runners.Parameterized.Parameters;
 
-import eu.bitwalker.useragentutils.Browser;
-import eu.bitwalker.useragentutils.OperatingSystem;
+import eu.bitwalker.useragentutils.*;
 
 public class ChromeMobileParameterizedTest extends
 		AbstractUserAgentParameterizedTest {
@@ -31,7 +30,17 @@ public class ChromeMobileParameterizedTest extends
 						Browser.CHROME_MOBILE, "16.0.912.75", OperatingSystem.ANDROID4 },
 				{
 						"Mozilla/5.0 (iPhone; U; CPU iPhone OS 5_1_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3",
-						Browser.CHROME_MOBILE, "19.0.1084.60", OperatingSystem.iOS5_IPHONE } 
+						Browser.CHROME_MOBILE, "19.0.1084.60", OperatingSystem.iOS5_IPHONE },
+				{
+						"Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/_BuildID_) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36",
+						Browser.ANDROID_WEB_VIEW, "4.0", OperatingSystem.ANDROID4 },
+				{
+						"Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36",
+						Browser.ANDROID_WEB_VIEW, "4.0", OperatingSystem.ANDROID5 },
+				{
+						"Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
+						Browser.ANDROID_WEB_KIT, "4.0", OperatingSystem.ANDROID4_TABLET }
+
 		});
 	}
 }
